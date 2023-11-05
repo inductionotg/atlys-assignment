dab452876178faf228833deb92bc117aec5022cd
inductionotg <ritesh2000.sinha@gmail.com>
2023-11-05 11:04:55 +0530

    Configured for react-router

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { CloseProvider } from './Components/signin-context';
-
+import { BrowserRouter } from "react-router-dom"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <CloseProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      
     </CloseProvider>
     
   </React.StrictMode>
