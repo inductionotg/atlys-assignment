d24d40f60e5ba8536dd6818001ffa150454aa452
inductionotg <ritesh2000.sinha@gmail.com>
2023-11-05 09:16:26 +0530

    Added AvatarInfo and CommentBox component

--- src/Components/Comment.js
+++ src/Components/Comment.js
@@ -1,5 +1,18 @@
+import './Comment.css'
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import SignupModal from './SignupModal';
+import { useCloseModal } from './signin-context';
+import CommentBox from './CommentBox';
+import AvatarInfo from './AvatarInfo';
 const Comment = () =>{
-
+    const [postText, setPostText] = useState('');
+    //const [showModal,setShowModal] = useState(false)
+    const {showModal1,setShowModal1,showModal2,setShowModal2} = useCloseModal();
+    const openModal1 = () => {
+         setShowModal1(true)
+    }
     return(
         <div class="container">
             <div className='container-post'>
@@ -21,7 +34,14 @@ const Comment = () =>{
                     <div className='btn-container'>
                         <button onClick={openModal1}>Post</button>
                     </div>
-                    
+                </div>
+                <div className="post-form">
+                    <AvatarInfo  imageSrc="https://gravatar.com/userimage/242153183/913f47040f2be20c1cc01fa4664a65e4.jpeg?size=256" timestamp="2023-11-04T14:30:00"/>
+                    <CommentBox/>       
+                </div>
+                <div className="post-form">
+                    <AvatarInfo imageSrc="https://gravatar.com/userimage/242153183/89f39e17f5897e3466448e0782788b5c.jpeg?size=256" timestamp="2023-11-04T15:45:00"/>
+                    <CommentBox/>       
                 </div>
             </div>
         </div>
