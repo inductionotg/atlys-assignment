8111c1a5fd2b9fa457cedc75f716d7cd879d38f4
inductionotg <ritesh2000.sinha@gmail.com>
2023-11-05 11:05:55 +0530

    Added routes and routing for components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import LoginForm from './Components/LoginForm';
+import Comment from './Components/Comment';
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      
+       <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="/comment" element={<Comment/>} />
+       </Routes>
     </div>
   );
 }
