d61a856e5e412295be706eaa426931b603f18894
inductionotg <ritesh2000.sinha@gmail.com>
2023-11-05 09:03:50 +0530

    Created a new component SignupModal

---
+++ src/Components/SignupModal.js
@@ -0,0 +1,15 @@
+
+import './LoginModal.css'
+import Signup from "./Signup"
+const SignupModal=()=>{
+    return (
+        <div className="modal-container">
+            <div className="login-modal">
+                <Signup/>
+            </div>
+            
+        </div>
+    )
+}
+
+export default  SignupModal
