78cc0128a80331658dd4fd44f544883e3101d02e
inductionotg <ritesh2000.sinha@gmail.com>
2023-11-05 09:13:37 +0530

    Added textare and implemented the modal functionality

--- src/Components/Comment.js
+++ src/Components/Comment.js
@@ -1,5 +1,31 @@
 const Comment = () =>{
 
+    return(
+        <div class="container">
+            <div className='container-post'>
+                <div className='container-header text-left'>
+                    <h2>Hello Jane</h2>
+                    <p>How are you doing Today? Would you like to share something with the commnunity</p>
+                </div>
+                <div className="post-form">
+                    <h2 className="post-title-header text-left">Create Post</h2>
+                    <textarea
+                        placeholder="Write your post here..."
+                        value={postText}
+                        onChange={(e) => setPostText(e.target.value)}
+                    />
+                    {showModal1 && <SignupModal/>}
+                    <div className="message-icon">
+                        <FontAwesomeIcon icon={faPaperPlane} />
+                    </div>
+                    <div className='btn-container'>
+                        <button onClick={openModal1}>Post</button>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Comment
