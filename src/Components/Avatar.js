05584ce94443c1e1f421a771fcdbf8c255608c93
inductionotg <ritesh2000.sinha@gmail.com>
2023-11-05 08:54:27 +0530

    Created a new Component Avatar

---
+++ src/Components/Avatar.js
@@ -0,0 +1,10 @@
+const Avatar = ({image})=>{
+    return (
+        <img 
+            alt="avatar"
+            className='avatar'
+            src={image}
+        />
+    )
+}
+export default Avatar
